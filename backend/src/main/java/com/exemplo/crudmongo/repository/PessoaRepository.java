357066caa7ae92d7645f95baf31bcc4187bd3606
Arthur <arthurnegaodebsb@gmail.com>
2025-10-24 14:53:33 -0300

    Repositório curso adicionado

--- backend/src/main/java/com/exemplo/crudmongo/repository/PessoaRepository.java
+++ backend/src/main/java/com/exemplo/crudmongo/repository/PessoaRepository.java
@@ -3,7 +3,6 @@ package com.exemplo.crudmongo.repository;
 import com.exemplo.crudmongo.Model.Pessoa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.mongodb.repository.MongoRepository; remover esse código
 import org.springframework.stereotype.Repository;
 
 @Repository
