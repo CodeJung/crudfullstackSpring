367aef0ee9fa8ae352e5d23d03950b63a1955cac
Arthur <arthurnegaodebsb@gmail.com>
2025-10-24 16:11:42 -0300

    For loop adicionado para popular base de dados para entidade curso

--- backend/src/main/java/com/exemplo/crudmongo/config/DataLoader.java
+++ backend/src/main/java/com/exemplo/crudmongo/config/DataLoader.java
@@ -1,6 +1,8 @@
 package com.exemplo.crudmongo.config;
 
 import com.exemplo.crudmongo.Model.Pessoa;
+import com.exemplo.crudmongo.Model.Curso;
+import com.exemplo.crudmongo.repository.CursoRepository;
 import com.exemplo.crudmongo.repository.PessoaRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -13,16 +15,24 @@ import java.util.Locale;
 public class DataLoader {
 
     @Bean
-    CommandLineRunner loadDatabase(PessoaRepository repository) {
+    CommandLineRunner loadDatabase(PessoaRepository pessoaRepository, CursoRepository cursoRepository) {
         return args -> {
-            if (repository.count() == 0) {
+            if (pessoaRepository.count() == 0) {
                 Faker faker = new Faker(new Locale("pt-BR"));
 
                 for (int i = 0; i < 200; i++) {
                     Pessoa pessoa = new Pessoa();
                     pessoa.setNome(faker.name().fullName());
                     pessoa.setIdade(faker.number().numberBetween(18, 70));
-                    repository.save(pessoa);
+                    pessoaRepository.save(pessoa);
+                }
+
+                for (int i = 0; i < 200; i++) {
+                    Curso curso = new Curso();
+                    curso.setNome(faker.educator().course());
+                    curso.setCargaHoraria(faker.number().randomDouble(0, 0, 100));
+                    curso.setAtivo(faker.random().nextBoolean());
+                    cursoRepository.save(curso);
                 }
 
                 System.out.println("✅ Banco populado com 200 registros!");
