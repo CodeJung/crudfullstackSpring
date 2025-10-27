510b16d174b2ccbad1259c217a8afbde3585138a
Arthur <arthurnegaodebsb@gmail.com>
2025-10-27 10:12:07 -0300

    Implementação da interface UserDetails

--- backend/src/main/java/com/exemplo/crudmongo/Model/Pessoa.java
+++ backend/src/main/java/com/exemplo/crudmongo/Model/Pessoa.java
@@ -6,18 +6,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity  // Anotação JPA para indicar que esta classe é uma entidade
 @Table(name = "pessoas") // Define o nome da tabela no banco de dados
 // @Document(collection = "pessoa")   Remover esse código
-public class Pessoa {
+public class Pessoa implements UserDetails {
 
     @Id // Indica que este campo é o identificador único do documento
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Geração automática do ID
     private Long id ;
     private String nome;
     private int idade;
+    private Role role;
 
     public Pessoa() {
     }// Construtor padrão pois é necessário para o JPA e MongoDB funcionar corretamente 
@@ -39,10 +45,47 @@ public class Pessoa {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
