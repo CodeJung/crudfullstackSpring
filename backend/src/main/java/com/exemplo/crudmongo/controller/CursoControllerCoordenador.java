bac2fe566f21bacd077d8fc3afe8e63b8c6f341e
Arthur <arthurnegaodebsb@gmail.com>
2025-10-27 10:20:56 -0300

    Alteração no end point

--- backend/src/main/java/com/exemplo/crudmongo/controller/CursoControllerCoordenador.java
+++ backend/src/main/java/com/exemplo/crudmongo/controller/CursoControllerCoordenador.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cursos")
+@RequestMapping("/api/cursos/coordenador")
 @CrossOrigin(origins = "*")
 public class CursoControllerCoordenador {
 
