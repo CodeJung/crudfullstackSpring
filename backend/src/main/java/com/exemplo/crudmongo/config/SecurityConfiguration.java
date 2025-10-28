a0fd8277ce7f4d43e7f2091b1e9ceebe7532d1d4
Arthur <arthurnegaodebsb@gmail.com>
2025-10-28 13:47:46 -0300

    Arquivos para configuração, service e controller do spring security

---
+++ backend/src/main/java/com/exemplo/crudmongo/config/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.exemplo.crudmongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.exemplo.crudmongo.Model.Permission.*;
+import static com.exemplo.crudmongo.Model.Role.COORDENADOR;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+//
+//     private final SecurityFIlter securityFilter;
+//
+//     public SecurityConfig(SecurityFilter securityFilter) {
+//         this.securityFilter = securityFilter;
+//     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/cursos").hasRole("COORDENADOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/cursos").hasRole("COORDENADOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/cursos").hasRole("COORDENADOR")
+                        .anyRequest().authenticated()
+                ).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
