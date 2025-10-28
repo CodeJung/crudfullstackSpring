6450bfff9d2f2aa60571df02fe9d27988b54254a
Arthur <arthurnegaodebsb@gmail.com>
2025-10-28 13:51:14 -0300

    Novo metodo findByEmail para validação de usuario

--- backend/src/main/java/com/exemplo/crudmongo/repository/PessoaRepository.java
+++ backend/src/main/java/com/exemplo/crudmongo/repository/PessoaRepository.java
@@ -3,10 +3,12 @@ package com.exemplo.crudmongo.repository;
 import com.exemplo.crudmongo.Model.Pessoa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    UserDetails findByEmail(String login);
 }
 
 
